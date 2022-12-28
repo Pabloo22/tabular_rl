bf9aad5de5ee458a4676637fa45870a4e8f466f4
Jorge <jorgechedo@gmail.com>
2022-12-28 11:52:18 +0100

    Bugs fixed.

--- tabular_rl/agents/dynamic_programming.py
+++ tabular_rl/agents/dynamic_programming.py
@@ -8,7 +8,7 @@ class DynamicProgramming(RLAgent):
     def __init__(self, mdp: MarkovDecisionProcess):
         super().__init__(mdp)
         self.V0 = np.zeros(mdp.n_states)  # DEBE CAMBIAR EN CADA ITERACION
-        self.π = np.zeros(mdp.n_states).astype(int)  # DEBE CAMBIAR EN CADA ITERACION
+        self.policy = np.zeros(mdp.n_states).astype(int)  # DEBE CAMBIAR EN CADA ITERACION
 
     def select_action(self, state: int) -> int:
         pass
@@ -16,11 +16,11 @@ class DynamicProgramming(RLAgent):
     def fit(self) -> None:
 
         for _ in tqdm(range(100)):
-            aux = np.fromfunction(np.vectorize(lambda state, action: sum(mdp.transition_probabilities[action,state,:]*(mdp.reward_function[action,state,:] + mdp.discount*self.V0))), (mdp.n_states, mdp.n_actions), dtype=int)
-            pi = np.fromfunction(np.vectorize(lambda s, a: (a == π[s])), (mdp.n_states, mdp.n_actions), dtype=int)
+            aux = np.fromfunction(np.vectorize(lambda state, action: sum(self.env.transition_probabilities_matrix[action,state,:]*(self.env.immediate_reward_matrix[action,state,:] + self.env.discount*self.V0))), (self.env.n_states, self.env.n_actions), dtype=int)
+            pi = np.fromfunction(np.vectorize(lambda s, a: (a == policy[s])), (mdp.n_states, self.env.n_actions), dtype=int)
 
             self.V0 = np.ma.masked_array(aux, mask=np.logical_not(pi)).compressed()  # V_k+1
-            if np.array_equal(self.π, (self.π := aux.argmax(axis=1))):  # Si la politica no cambia, ha finalizado
+            if np.array_equal(self.policy, (self.policy := aux.argmax(axis=1))):  # Si la politica no cambia, ha finalizado
                 break
 
     def save_learning(self, path: str) -> None:
