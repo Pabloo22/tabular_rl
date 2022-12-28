031859cff8fee872970b8b359e58d8558c89a7a5
Pablo <pablete.arino@gmail.com>
2022-12-28 12:30:37 +0100

    Pseudocode DP

--- tabular_rl/agents/dynamic_programming.py
+++ tabular_rl/agents/dynamic_programming.py
@@ -1,29 +1,55 @@
 import numpy as np
-from tqdm import tqdm
+import tqdm
 from tabular_rl.core import RLAgent, MarkovDecisionProcess
 
 
 class DynamicProgramming(RLAgent):
 
-    def __init__(self, mdp: MarkovDecisionProcess):
-        super().__init__(mdp)
-        self.V0 = np.zeros(mdp.n_states)  # DEBE CAMBIAR EN CADA ITERACION
-        self.policy = np.zeros(mdp.n_states).astype(int)  # DEBE CAMBIAR EN CADA ITERACION
+    def __init__(self, env: MarkovDecisionProcess):
+        super().__init__(env)
+        self.state_value_array = None
+        self.policy = None
 
     def select_action(self, state: int) -> int:
         pass
 
-    def fit(self) -> None:
+    def initialize(self) -> None:
+        self.state_value_array = np.zeros(mdp.n_states)
+        self.policy = np.zeros(mdp.n_states, dtype=int)
 
-        for _ in tqdm(range(100)):
-            aux = np.fromfunction(np.vectorize(lambda state, action: sum(self.env.transition_probabilities_matrix[action,state,:]*(self.env.immediate_reward_matrix[action,state,:] + self.env.discount*self.V0))), (self.env.n_states, self.env.n_actions), dtype=int)
-            pi = np.fromfunction(np.vectorize(lambda s, a: (a == self.policy[s])), (mdp.n_states, self.env.n_actions), dtype=int)
+    def policy_evaluation(self, tol: float = 0.001, n_evaluations: int = 1000) -> bool:
+        pass
 
-            self.V0 = np.ma.masked_array(aux, mask=np.logical_not(pi)).compressed()  # V_k+1
-            if np.array_equal(self.policy, aux.argmax(axis=1)):  # Si la politica no cambia, ha finalizado
+    def policy_improvement(self) -> bool:
+        pass
+
+    # def fit(self, tol=0.001, max_evaluations=1, max_iters=100_000, use_tqdm=True) -> None:
+    #
+    #     for _ in tqdm.tqdm(range(100), disable=not use_tqdm):
+    #
+    #         for _ in range(max_evaluations):
+    #             aux = np.fromfunction(np.vectorize(lambda state, action: sum(self.env.transition_probabilities_matrix[action,state,:]*(self.env.immediate_reward_matrix[action,state,:] + self.env.discount*self.V0))), (self.env.n_states, self.env.n_actions), dtype=int)
+    #             pi = np.fromfunction(np.vectorize(lambda s, a: (a == self.policy[s])), (mdp.n_states, self.env.n_actions), dtype=int)
+    #
+    #             self.V0 = np.ma.masked_array(aux, mask=np.logical_not(pi)).compressed()  # V_k+1
+    #         if np.array_equal(self.policy, aux.argmax(axis=1)):  # Si la politica no cambia, ha finalizado
+    #             break
+    #
+    #         self.policy = aux.argmax(axis=1)
+
+    def fit(self,
+            tol: float = 0.001,
+            max_evaluations: int = 1,
+            max_iters: int = 100_000,
+            use_tqdm: bool = True) -> None:
+
+        for _ in tqdm.tqdm(range(max_iters), disable=not use_tqdm):
+
+            values_stable = self.policy_evaluation(tol, max_evaluations)
+            policy_stable = self.policy_improvement()
+
+            if values_stable and policy_stable:
                 break
-
-            self.policy = aux.argmax(axis=1)
 
     def save_learning(self, path: str) -> None:
         pass
@@ -52,4 +78,4 @@ if __name__ == '__main__':
     mdp = MarkovDecisionProcess(3, 2, transition_probabilities, reward_function, 1)
     agent = DynamicProgramming(mdp)
     agent.fit()
-    print(agent.policy, agent.V0)
+    print(agent.policy, agent.state_value_array)
